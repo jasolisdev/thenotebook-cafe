cca6b554e468c2de5ef93db8174fa886f483ebd2
Jose Solis <57308627+jasolisdev@users.noreply.github.com>
2025-12-20 07:39:12 -0800

    Fix e2e build dependencies

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,8 +1,15 @@
 import { createClient } from "next-sanity";
 
+export const hasSanityConfig = Boolean(
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID && process.env.NEXT_PUBLIC_SANITY_DATASET
+);
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "missing-project-id";
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? "missing-dataset";
+
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId,
+  dataset,
   apiVersion: "2025-01-01",
   useCdn: true,
 });
--- sanity/lib/writeClient.ts
+++ sanity/lib/writeClient.ts
@@ -1,8 +1,11 @@
 import { createClient } from "next-sanity";
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "missing-project-id";
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? "missing-dataset";
+
 export const writeClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId,
+  dataset,
   apiVersion: "2025-01-01",
   token: process.env.SANITY_WRITE_TOKEN, // Editor or higher
   useCdn: false,
